ab242b08b4ea4d7eb85d62e775da1c5fc45538c0
Luca Di Stefano <lucadistefano90@gmail.com>
2021-12-28 18:46:52 +0100

    Reactigospace version of case study 1

---
+++ src/react_instances/template.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"sync"
+	"time"
+	"math/rand"
+	. "github.com/pspaces/gospace"
+)
+
+var debug bool
+var l sync.Mutex
+
+var writes_local int
+var writes_remote int
+var reads_local int
+var reads_remote int
+var writes_replicated int
+var writestotal int
+var reads_success int
+var reads_insuccess int
+var writes_extra int
+var wg sync.WaitGroup
+
+var s1 Space
+var s2 Space
+var s3 Space
+var s4 Space
+
+
+func main() {
+	debug = false
+	writes_local = 0
+	writes_remote = 0
+	reads_local = 0
+	reads_remote = 0
+	writes_replicated = 0
+	reads_success = 0
+	reads_insuccess = 0
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	wg.Add(4)
+
+	s1 = NewSpace("tcp://localhost:34001/s1")
+	s2 = NewSpace("tcp://localhost:34002/s2")
+	s3 = NewSpace("tcp://localhost:34003/s3")
+	s4 = NewSpace("tcp://localhost:34004/s4")
+    
+
+	go p1()
+	go p2()
+	go p3()
+	go p4()
+   
+
+	wg.Wait()
+
+	writes_replicated = 0   // these include the normal put too
+
+	fmt.Println("       loc w,    rem w,   repl w,    loc r,    rem r,    tot w,   succ r,   fail r")
+	fmt.Printf ("    %8d, %8d, %8d, %8d, %8d, %8d, %8d, %8d\n", writes_local, writes_remote, writes_replicated, reads_local, reads_remote, writestotal, reads_success, reads_insuccess)
+}
+
+func p1() {
+	defer wg.Done()
+	var value int
+
+	<P1>
+}
+
+func p2() {
+	defer wg.Done()
+	var value int
+
+	<P2>
+}
+
+func p3() {
+	defer wg.Done()
+	var value int
+
+	<P3>
+}
+
+func p4() {
+	defer wg.Done()
+	var value int
+
+	<P4>
+}
+
+
+func log(format string, a ...interface{}) {
+	if debug {
+    	fmt.Fprintf(os.Stdout, format+"\n", a ...)
+    }
+}
+
+func delay() {
+	time.Sleep(time.Duration(rand.Int63n(75))*time.Millisecond)
+}
+
+func countreads(value int, localspace int, targetspace int) {
+	if value == -1 {
+		reads_insuccess+=1
+	} else {
+		reads_success+=1
+	}
+
+	if localspace == targetspace {
+		reads_local+=1
+	} else {
+		reads_remote+=1
+	}
+}
+
+func countwrites(value int, localspace int, targetspace int) {
+	writestotal+=1
+
+	if localspace == targetspace {
+		writes_local+=1
+	} else {
+		writes_remote+=1
+	}
+}
+
+
+
+
